f5c7333b717fbce96ee2f6f9a0da100b02625db7
Takuya Narihira <dvbscb@gmail.com>
2015-03-11 16:21:35 -0700

    Expose CUDA_BLOCKS CUDA_NUM_THREADS to Python

--- python/caffe/__init__.py
+++ python/caffe/__init__.py
@@ -3,6 +3,7 @@ from ._caffe import (
     set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver,
     get_device,
     check_mode_cpu, check_mode_gpu,
+    get_cuda_num_threads, get_blocks,
 )
 from .proto.caffe_pb2 import TRAIN, TEST
 from .classifier import Classifier
