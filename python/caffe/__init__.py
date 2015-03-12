77c1fc99bbae26300a20b3b166b6d0f8715563e6
Takuya Narihira <dvbscb@gmail.com>
2015-03-11 18:04:45 -0700

    Fix: CPU_ONLY

--- python/caffe/__init__.py
+++ python/caffe/__init__.py
@@ -3,9 +3,12 @@ from ._caffe import (
     set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver,
     get_device,
     check_mode_cpu, check_mode_gpu,
-    get_cuda_num_threads, get_blocks,
 )
 from .proto.caffe_pb2 import TRAIN, TEST
 from .classifier import Classifier
 from .detector import Detector
 import io
+try:
+	from ._caffe import get_cuda_num_threads, get_blocks
+except ImportError:
+	pass
